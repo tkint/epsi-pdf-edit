f746e3a1cc82ad3835a65de61f2170b3c44b9d9b
t.kint <t.kint@hotmail.fr>
2016-10-25 14:46:27 +0200

    Séparation des tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import java.io.File;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 import java.io.IOException;
@@ -23,60 +22,98 @@ public class Main {
 
     public static void main(String[] args) throws IOException
     {
-        // Instanciation des controllers
-        TextController textController = new TextController();
-        ImageController imageController = new ImageController();
-        TableController tableController = new TableController();
+        // Instanciation du programme
+        Main main = new Main();
         
         try
         {
             // Instanciation du document à créer
             PDDocument document = new PDDocument();
             
-            // Ajout de deux pages
-            document.addPage(new PDPage());
-            document.addPage(new PDPage());
+            // Sauvegarde du document
+            document.save(docTitle + ".pdf");
+            
+            //main.testAddImage();
+            //main.testExtractImage();
+            main.testAddTable();
+        } catch(IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+    
+    private void testAddImage() {
+        try {
+            ImageController imageController = new ImageController();
+            
+            // Chargement du document
+            File file = new File(docTitle + ".pdf");
+            PDDocument document = PDDocument.load(file);
+            
             document.addPage(new PDPage());
             
-            // Ajout du texte sur la première page (index commence à 0)
-            textController.addText(document, document.getPage(0), text, 100, 700, 15, PDType1Font.COURIER);
-
+            PDPageContentStream contentStream = new PDPageContentStream(document, document.getPage(document.getNumberOfPages() - 1), PDPageContentStream.AppendMode.APPEND, true);
+            
             // Ajout d'une image sur la deuxième page
-            imageController.addImage(document, document.getPage(1), img, 100, 400, 0.2f);
+            imageController.addImage(document, contentStream, img, 100, 400, 0.2f);
+            
+            contentStream.close();
+            
+            document.save(docTitle + ".pdf");
+        } catch(IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+    
+    private void testExtractImage() {
+        try {
+            ImageController imageController = new ImageController();
+            
+            // Chargement du document
+            File file = new File(docTitle + ".pdf");
+            PDDocument document = PDDocument.load(file);
+
+            // Extraction de l'image définie dans le format défini
+            imageController.extractImageByName(document, "Im1", "png");
+            
+            // Fermeture du document
+            document.close();
+        } catch(IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+    
+    private void testAddTable() {
+        try {
+            TableController tableController = new TableController();
+            
+            // Chargement du document
+            File file = new File(docTitle + ".pdf");
+            PDDocument document = PDDocument.load(file);
+            
+            document.addPage(new PDPage());
+            
+            PDPageContentStream contentStream = new PDPageContentStream(document, document.getPage(document.getNumberOfPages() - 1), PDPageContentStream.AppendMode.APPEND, true);
             
             // Instanciation d'un tableau
             Table table = new Table(0, 100, 600, 200, 200);
             // Génération du tableau
             table.generateTable(2, 7);
+            table.addColumns(2);
             
             // Récupération d'une cellule
-            Cell cell1 = table.getCell(5);
+            Cell cell = table.getCell(0);
             // Initialisation du contenu
-            cell1.setContent("TEST");
-            // Calcul de la position du texte
-            float posX = cell1.getPosX() + (cell1.getWidth() / 2);
-            float posY = cell1.getPosY() - (cell1.getHeight() / 2);
+            cell.setContent("TEST");
+            // Affichage du contenu dans la cellule
+            tableController.displayCellContent(contentStream, cell, "center", "middle");
             
-            // Affichage du texte dans la cellule
-            textController.addText(document, document.getPage(0), cell1.getContent(), posX, posY, 12, PDType1Font.COURIER);
-            
-            // Check du tableau
-            System.out.println(table);
-                    
             // Ajout d'un tableau
-            tableController.drawTable(document, document.getPage(0), table);
+            tableController.drawTable(contentStream, table);
             
-            // Sauvegarde du document
+            contentStream.close();
             document.save(docTitle + ".pdf");
-            
-            // Chargement du document
-            File file = new File(docTitle + ".pdf");
-            PDDocument doc2 = PDDocument.load(file);
-            
-            // Extraction de l'image définie dans le format défini
-            imageController.extractImageByName(doc2, "Im1", "png");
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch(IOException e) {
+            System.out.println(e.toString());
         }
     }
 }
