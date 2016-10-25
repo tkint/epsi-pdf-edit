e7114f33e561a3d2c98f7b2eb671cc475d75f040
t.kint <t.kint@hotmail.fr>
2016-10-25 14:48:52 +0200

    Modification des appels Ajout de displayCellContent Simplification du drawTable

--- src/main/java/controller/TableController.java
+++ src/main/java/controller/TableController.java
@@ -5,59 +5,120 @@
  */
 package controller;
 
+import java.io.IOException;
 import model.table.Cell;
 import model.table.Row;
 import model.table.Table;
-import org.apache.pdfbox.contentstream.PDContentStream;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 /**
  * Created by Thomas on 21/10/2016.
  * @author t.kint
  */
 public class TableController {
-    public void drawTable(PDDocument document, PDPage page, Table table) {
+    /**
+     * Affiche le contenu d'une cellule
+     * @param contentStream
+     * @param cell
+     * @param alignX
+     * @param alignY 
+     */
+    public void displayCellContent(PDPageContentStream contentStream, Cell cell, String alignX, String alignY) {
         try {
-            PDPageContentStream content = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true);
-            
-            float x = table.getPosX();
-            float y = table.getPosY();
-            for (Row row : table.getRows()) {
-                for (Cell cell : row.getCells()) {
-                    drawRectangle(content, x, y, cell.getWidth(), cell.getHeight());
-                    x += cell.getWidth();
-                }
-                y -= row.getHeight();
-                x = table.getPosX();
+            TextController textController = new TextController();
+
+            float posX = cell.getPosX();
+            float posY = cell.getPosY();
+
+            switch(alignX) {
+                case "left":
+                    posX += 3;
+                    break;
+                case "center": 
+                    posX += (cell.getWidth() - cell.getContent().length() * 7) / 2;
+                    break;
+                case "right":
+                    posX += cell.getWidth() - cell.getContent().length() * 7 - 3;
+                    break;
+                default:
+                    posX += (cell.getWidth() - cell.getContent().length() * 7) / 2;
+                    break;
             }
 
-            content.close();
-        } catch(Exception e) {
-            e.printStackTrace();
+            switch(alignY) {
+                case "top":
+                    posY -= cell.getHeight() - PDType1Font.COURIER.getBoundingBox().getHeight() / 1000 * 12 - 5;
+                    break;
+                case "middle": 
+                    posY -= (cell.getHeight() + PDType1Font.COURIER.getBoundingBox().getHeight() / 1000 * 6) / 2;
+                    break;
+                case "bottom":
+                    posY -= cell.getHeight() - PDType1Font.COURIER.getBoundingBox().getHeight() / 1000 - 3;
+                    break;
+                default:
+                    posY -= (cell.getHeight() + PDType1Font.COURIER.getBoundingBox().getHeight() / 1000 * 6) / 2;
+                    break;
+            }
+            
+            // Affichage du texte dans la cellule
+            textController.addText(contentStream, cell.getContent(), posX, posY, 12, PDType1Font.COURIER);
+        } catch(IOException e) {
+            System.out.println(e.toString());
         }
     }
     
-    private void drawRectangle(PDPageContentStream content, float posX, float posY, float width, float height) {
+    /**
+     * Dessine un tableau
+     * @param contentStream
+     * @param table 
+     */
+    public void drawTable(PDPageContentStream contentStream, Table table) {
         try {
-            drawLine(content, posX, posY, posX + width, posY);
-            drawLine(content, posX, posY, posX, posY - height);
-            drawLine(content, posX + width, posY, posX + width, posY - height);
-            drawLine(content, posX, posY - height, posX + width, posY - height);
+            for (Row row : table.getRows()) {
+                for (Cell cell : row.getCells()) {
+                    drawRectangle(contentStream, cell.getPosX(), cell.getPosY(), cell.getWidth(), cell.getHeight());
+                }
+            }
         } catch(Exception e) {
-            e.printStackTrace();
+            System.out.println(e.toString());
         }
     }
     
-    private void drawLine(PDPageContentStream content, float posX, float posY, float posLastX, float posLastY) {
+    /**
+     * Dessine un rectangle
+     * @param contentStream
+     * @param posX
+     * @param posY
+     * @param width
+     * @param height 
+     */
+    private void drawRectangle(PDPageContentStream contentStream, float posX, float posY, float width, float height) {
         try {
-            content.moveTo(posX, posY);
-            content.lineTo(posLastX, posLastY);
-            content.stroke();
+            drawLine(contentStream, posX, posY, posX + width, posY);
+            drawLine(contentStream, posX, posY, posX, posY - height);
+            drawLine(contentStream, posX + width, posY, posX + width, posY - height);
+            drawLine(contentStream, posX, posY - height, posX + width, posY - height);
         } catch(Exception e) {
-            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+    }
+    
+    /**
+     * Dessine une ligne
+     * @param contentStream
+     * @param posX
+     * @param posY
+     * @param posLastX
+     * @param posLastY 
+     */
+    private void drawLine(PDPageContentStream contentStream, float posX, float posY, float posLastX, float posLastY) {
+        try {
+            contentStream.moveTo(posX, posY);
+            contentStream.lineTo(posLastX, posLastY);
+            contentStream.stroke();
+        } catch(IOException e) {
+            System.out.println(e.toString());
         }
     }
 }
