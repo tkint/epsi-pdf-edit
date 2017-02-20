9f6f21e04da1e7e0bbf7e5869f11b2a0ab13181d
Thomas <thomas.kint@epsi.fr>
2017-02-20 11:27:28 +0100

    Correction des calculs de positionnement d'image. Correction des calculs de retaille de l'image Ajout du path dans ImagePDF Ajout de AddImage en fonction de ImagePDF sur ImageController Ajout du menu contextuel dans l'ajout d'image

---
+++ src/main/java/src/model/ImagePDF.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.model;
+
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author gcapel
+ */
+public class ImagePDF {
+
+    private int id;
+    private float posX;
+    private float posY;
+    private float width;
+    private float height;
+    private Image image;
+    private String path;
+
+    public ImagePDF(int id, Image image, float posX, float posY, float width, float height, String path) {
+        this.id = id;
+        this.image = image;
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+        this.path = path;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getPosX() {
+        return posX;
+    }
+
+    public void setPosX(float posX) {
+        this.posX = posX;
+    }
+
+    public float getPosY() {
+        return posY;
+    }
+
+    public void setPosY(float posY) {
+        this.posY = posY;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void refreshPos(float posX, float posY, float width, float height) {
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+    }
+}
